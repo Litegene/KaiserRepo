94f2feb1110e5a4496386744534b224eb83a69d3
Charlie <revoflow7@gmail.com>
2022-04-03 11:33:56 +0200

    Spring project setup

---
+++ springKaiser/src/main/java/com/example/springKaiser/YoutubeFeedManagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.springKaiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeFeedManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeFeedManagerApplication.class, args);
+		System.out.println("lol");
+	}
+
+}

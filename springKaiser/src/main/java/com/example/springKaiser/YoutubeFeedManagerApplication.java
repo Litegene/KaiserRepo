06731d310a8e1bf295857a4d036161efb8da65a1
Litegene <khchang260@gmail.com>
2022-04-04 20:51:53 +0200

    Test run

--- springKaiser/src/main/java/com/example/springKaiser/YoutubeFeedManagerApplication.java
+++ springKaiser/src/main/java/com/example/springKaiser/YoutubeFeedManagerApplication.java
@@ -9,6 +9,12 @@ public class YoutubeFeedManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YoutubeFeedManagerApplication.class, args);
 		System.out.println("lol");
+		int youtubeFeedCount = this.returnYoutubeFeed();
+		System.out.println(youtubeFeedCount);
+	}
+
+	public static int returnYoutubeFeed(){
+		return 10;
 	}
 
 }

a8f52a6a7958cd54394a327d901b579f4f8ba3c4
Litegene <khchang260@gmail.com>
2022-05-14 19:51:19 +0200

    DTO homework

--- backend/kaiser/src/main/java/com/example/springKaiser/controller/VideoController.java
+++ backend/kaiser/src/main/java/com/example/springKaiser/controller/VideoController.java
@@ -1,5 +1,6 @@
 package com.example.springKaiser.controller;
 
+import com.example.springKaiser.business.video.VideoDto;
 import com.example.springKaiser.business.video.VideoService;
 import com.example.springKaiser.entities.Video;
 import com.example.springKaiser.repositories.VideoRepository;
@@ -22,9 +23,24 @@ public class VideoController {
         //Post is like a save, but more data involved
         //Put is an update I think? google it bro
 
+//        @GetMapping("/videoList")
+//        public List<Video> listVideo() {
+//            return videoService.listVideo();
+//        }
+
+//        @GetMapping("/videoList")
+//        public String listVideo(@RequestBody VideoDto videoDto) {
+//            return videoService.listVideo(videoDto);
+//        }
+
         @GetMapping("/videoList")
         public List<Video> listVideo() {
             return videoService.listVideo();
+        }
+
+        @GetMapping("/videoListDto")
+        public List<VideoDto> listVideoDto() {
+            return videoService.listVideoDto();
         }
 
         @GetMapping("/listVideoByChannel/{channelName}")
@@ -32,8 +48,18 @@ public class VideoController {
             return videoService.listByChannel(channelName);
         }
 
+        @GetMapping("/listVideoByChannelDto/{channelId}")
+        public List<VideoDto> listByChannelDto(@PathVariable int channelId){
+            return videoService.listByChannelDto(channelId);
+        }
+
         @PostMapping("/saveVideo")
         public void saveVideo(@RequestBody Video video){
             videoService.saveVideo(video);
-    }
+        }
+
+        @PostMapping("/saveVideoDto")
+        public String saveVideoDto(@RequestBody VideoDto videoDto){
+            return videoService.saveVideos(videoDto);
+        }
 }
---
+++ backend/kaiser/src/main/resources/db/changelog/scripts/17_addchannelid.sql
@@ -0,0 +1,6 @@
+update videotable set channelid = 1 where id = 1;
+update videotable set channelid = 2 where id = 2;
+update videotable set channelid = 2 where id = 3;
+update videotable set channelid = 3 where id = 4;
+update videotable set channelid = 4 where id = 5;
+update videotable set channelid = 4 where id = 6;

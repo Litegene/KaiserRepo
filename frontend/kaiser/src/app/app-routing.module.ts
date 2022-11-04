19d24275f9b3e88dd75a5d93962f6f8150ced697
Matthew <matthew.dragonmaster@gmail.com>
2022-11-04 05:57:27 +0200

    Frontend

--- frontend/kaiser/src/app/app-routing.module.ts
+++ frontend/kaiser/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { BookDataComponent } from './book-data/book-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books', component: BookDataComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
